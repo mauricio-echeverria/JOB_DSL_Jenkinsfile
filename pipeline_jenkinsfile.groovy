f97cefe286d18a6ccef40ce6c250f806733fbc28
mauricio-echeverria <48423074+mauricio-echeverria@users.noreply.github.com>
2019-03-11 11:32:35 -0300

    Update pipeline_jenkinsfile.groovy

--- pipeline_jenkinsfile.groovy
+++ pipeline_jenkinsfile.groovy
@@ -25,7 +25,7 @@ pipelineJob(proyect_client + '/' + project_name + '/' + 'Desarrollo' + '/' + pro
         cpsScm {
             scm {
                 git(url_git)
-                branches(branch_scm)
+                branches("*/master")
                 //credentials(credentials_scm)
             }
 
