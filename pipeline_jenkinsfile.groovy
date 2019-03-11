62c8c03e449a89e2d180c1b7a4baaab6de6754b7
mauricio-echeverria <48423074+mauricio-echeverria@users.noreply.github.com>
2019-03-11 11:04:10 -0300

    Update pipeline_jenkinsfile.groovy

--- pipeline_jenkinsfile.groovy
+++ pipeline_jenkinsfile.groovy
@@ -5,7 +5,7 @@
  // Variables locales (Propias de la clase)
  def url_git = "${URL_SCM}"
  def branch_scm = "${BRANCH_SCM}"
- def credentials_scm = "${CREDENTIALS_SCM}"
+ def credentials_scm = "calidad.jenkins"
 
  folder(proyect_client) {
      description('Cliente ' + proyect_client)
