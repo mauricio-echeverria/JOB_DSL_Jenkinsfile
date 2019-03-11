484d056d6d989526a83f1a6a50f2e46384353f63
mauricio-echeverria <48423074+mauricio-echeverria@users.noreply.github.com>
2019-03-11 11:22:00 -0300

    Update pipeline_jenkinsfile.groovy

--- pipeline_jenkinsfile.groovy
+++ pipeline_jenkinsfile.groovy
@@ -1,6 +1,5 @@
  def proyect_client = "${PROJECT_CLIENT}"
  def project_name = "${PROJECT_NAME}"
- def project_description = "${PROJECT_DESCRIPTION}"
 
  // Variables locales (Propias de la clase)
  def url_git = "${URL_SCM}"
